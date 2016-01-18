fcb851b9fb79eb475a3f10a35b873d544ca82ac0
Sergey <sera.prokop@gmail.com>
2016-01-18 11:05:25 +0300

    change folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,41 @@
-var gulp        = require('gulp');
-var browserSync = require('browser-sync').create();
-var sass        = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
-var csscomb = require('gulp-csscomb');
+var gulp = require('gulp'),
+    browserSync = require('browser-sync').create(),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    csscomb = require('gulp-csscomb'),
+    imagemin = require('gulp-imagemin'),
+    pngquant = require('imagemin-pngquant');
 
 gulp.task('serve', ['sass'], function() {
 
     browserSync.init({
-        server: "./"
+        server: "./src/"
     });
 
-    gulp.watch("./scss/**/*.scss", ['sass']);
-    gulp.watch("./*.html").on('change', browserSync.reload);
+    gulp.watch("./src/scss/**/*.scss", ['sass']);
+    gulp.watch("./src/*.html").on('change', browserSync.reload);
 });
 
 gulp.task('sass', function() {
-    return gulp.src("./scss/main.scss")
+    return gulp.src("./src/scss/main.scss")
         .pipe(sass().on('error', sass.logError))
         .pipe(csscomb())
         .pipe(autoprefixer({
       browsers: ['last 2 versions'],
       cascade: false
     }))
-        .pipe(gulp.dest("./css"))
+        .pipe(gulp.dest("./src/css"))
         .pipe(browserSync.stream());
+});
+
+gulp.task('img', () => {
+    return gulp.src('src/img/*')
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
+        }))
+        .pipe(gulp.dest('src/img'));
 });
 
 gulp.task('default', ['serve']);
